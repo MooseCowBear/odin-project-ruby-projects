9bb86b775452255048df215c66396a55f3085afa
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-22 12:16:33 -0400

    Debug

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -7,7 +7,7 @@ class Player
 end
 
 class Game
-  def initialize #start new game by calling Game.new in main program
+  def initialize 
     @player1 = get_player(1);
     @player2 = get_player(2);
     @board = get_starting_board
@@ -16,19 +16,17 @@ class Game
     @remaining_squares = (1..9).to_a
   end
 
-  def play_game #this is called, where the looping happens etc
-
+  def play_game 
     until winner || remaining_squares.length == 0
+      puts "The current board is: "
+      print_board
       move = get_move
       update_board(move)
-      done = check_winner(move)
-      puts "Done: #{done}"
+      done = check_winner(move) 
       if done 
         self.winner = curr_player > 1 ? player2 : player1 
       end
       update_curr_player
-      puts "The current board is: "
-      pp board
     end
     announce_result
   end
@@ -48,18 +46,14 @@ class Game
     board = Array.new(3) { Array.new(3) }
     3.times do |i|
       3.times do |j|
-        puts "#{i}, #{j}"
         board[i][j] = 3 * i + j + 1 #should result in 1 - 9
-        pp board
       end
     end
-    pp board
     board
   end
 
   def update_curr_player 
     self.curr_player = (curr_player % 2) + 1
-    puts "new value for whose up is #{curr_player}"
   end
 
   def get_move
@@ -67,26 +61,18 @@ class Game
     
     loop do 
       puts "Enter move for #{player.name}: " 
-      begin 
-        move = gets.chomp.to_i
-        if move.between?(1, 9)
-          self.remaining_squares.delete(move)
-          puts "remaining squares are: "
-          pp remaining_squares
-          move = convert_move(move)
-          return move
-        end
-      rescue TypeError => e
-        puts "Remaining squares are #{self.remaining_squares.join(", ")}."
+      move = gets.chomp.to_i
+      if remaining_squares.include?(move)
+        self.remaining_squares.delete(move)
+        move = convert_move(move)
+        return move
       end
     end
   end
 
   def convert_move(num) #take number 1 - 9, and convert to indices
     j = (num - 1) % 3
-    puts "j: #{j}"
     i = (num - (j + 1)) / 3
-    puts "i: #{i}"
     [i, j]
   end
 
@@ -99,8 +85,7 @@ class Game
     mark = get_mark
     row = move[0] 
     col = move[1]
-    self.board[row][col] = mark
-    pp board 
+    board[row][col] = mark
   end
 
   def check_winner(move) 
@@ -131,6 +116,7 @@ class Game
   def winning_diagonal?(move, mark) 
     unless move[0] == move[1] || (move[0] + move[1] == 2)
       return false
+    end
     left_right = true
     3.times do |i|
       unless board[i][i] == mark
@@ -162,7 +148,9 @@ class Game
     end
   end
 
-end
+  def print_board
+    board.each { |elem| pp elem }
+  end
 
 end
 
