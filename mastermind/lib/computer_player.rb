dfcaa98252e59f65477a5ad83f30f1cda9587386
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-01 08:21:42 -0500

    add computer player

---
+++ mastermind/lib/computer_player.rb
@@ -0,0 +1,80 @@
+require_relative "./player.rb"
+require_relative "./feedback.rb"
+
+class ComputerPlayer < Player
+  include Feedback 
+
+  attr_accessor :possible_codes, :impossible_codes, :guess
+
+  def initialize
+    super("Hal")
+    @possible_codes = generate_codes
+    @impossible_codes = []
+    @guess = [1, 1, 2, 2] # starting guess
+  end
+
+  def make_guess(prev_feedback)
+    if impossible_codes.length > 0
+      update_codes(prev_feedback)
+
+      best_pos = best_guess_from(possible_codes)
+      best_impos = best_guess_from(impossible_codes)
+
+      if best_impos.score < best_pos.score
+        self.guess = best_impos.guess
+      else 
+        self.guess = best_pos.guess
+      end
+    end
+    guess 
+  end
+
+  def set_code 
+    possible_codes.sample
+  end
+
+  private
+
+  def generate_codes
+    # all 1296 possible codes
+    s = Array.new(4, [1, 2, 3, 4, 5, 6])
+    s[1..-1].reduce(s[0]){ |m,v| m = m.product(v).map(&:flatten) }
+  end
+
+  def update_codes(prev_feedback)
+    keep, discard = possible_codes.partition { |code| consistent?(prev_feedback, code) }
+
+    self.possible_codes = keep
+    self.impossible_codes = impossible_codes + discard
+  end
+
+  def consistent?(prev_feedback, code)
+    feedback(code, prev_guess) == prev_feedback # would this code give me the feedback I got?
+  end
+
+  def score(hypothetical_guess)
+    # calculate the worst outcome for this guess
+    outcome_tallies = {}
+
+    possible_codes.each do |code|
+      outcome_tallies[feedback(code, hypothetical_guess)] += 1
+    end
+    outcome_tallies.max_by{ |key, val| val } # worst case is the greatest number of possible remaining codes
+  end
+
+  Guess = Struct.new(:guess, :score)
+
+  def best_guess_from(codes)
+    min_score = 1296
+    best_guess = nil
+
+    codes.each do |code|
+      score = score(code)
+      if score < min_score
+        min_score = score
+        best_guess = code
+      end
+    end
+    Guess.new(guess: best_guess, score: min_score)
+  end
+end
---
+++ mastermind/spec/computer_player_spec.rb
@@ -0,0 +1,63 @@
+require_relative '../lib/computer_player.rb'
+
+describe ComputerPlayer do
+  subject(:test_player) { described_class.new }
+
+  describe("#initialize") do
+    it "generates all 1296 codes and holds them in possible codes" do
+      expect(test_player.possible_codes.length).to eq 1296
+    end
+  end
+
+  describe("#set_code") do
+    it "returns a code from possible codes" do
+      res = test_player.set_code
+      expect(test_player.possible_codes.include?(res)).to be true
+    end
+  end
+
+  describe("#make_guess") do
+    it "returns initial guess if impossible codes is empty" do
+      expect(test_player.make_guess(nil)).to eq [1, 1, 2, 2]
+    end
+
+    it "returns best possible code if it has a lower score than best impossible code and updates player's guess" do
+      allow(test_player.impossible_codes).to receive(:length).and_return(200)
+      allow(test_player).to receive(:update_codes)
+
+      best_pos = double(score: 10, guess: [1, 1, 1, 1])
+      best_impos = double(score: 20, guess: [2, 2, 2, 2])
+
+      allow(test_player).to receive(:best_guess_from).and_return(best_pos, best_impos)
+      res = test_player.make_guess("")
+      expect(res).to eq best_pos.guess
+      expect(test_player.guess).to eq best_pos.guess 
+    end
+
+    it "returns best possible code if it has score equal to best impossible code" do
+      allow(test_player.impossible_codes).to receive(:length).and_return(200)
+      allow(test_player).to receive(:update_codes)
+
+      best_pos = double(score: 10, guess: [1, 1, 1, 1])
+      best_impos = double(score: 10, guess: [2, 2, 2, 2])
+
+      allow(test_player).to receive(:best_guess_from).and_return(best_pos, best_impos)
+      res = test_player.make_guess("")
+      expect(res).to eq best_pos.guess
+      expect(test_player.guess).to eq best_pos.guess 
+    end
+
+    it "returns best impossible code if it has a score lower than best possible code" do
+      allow(test_player.impossible_codes).to receive(:length).and_return(200)
+      allow(test_player).to receive(:update_codes)
+
+      best_pos = double(score: 20, guess: [1, 1, 1, 1])
+      best_impos = double(score: 10, guess: [2, 2, 2, 2])
+
+      allow(test_player).to receive(:best_guess_from).and_return(best_pos, best_impos)
+      res = test_player.make_guess("")
+      expect(res).to eq best_impos.guess
+      expect(test_player.guess).to eq best_impos.guess 
+    end
+  end
+end
