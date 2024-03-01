2cd11164bdaba0403e71ce4b9e458c6f67727aec
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-01 08:45:10 -0500

    fix expectations

--- mastermind/spec/human_player_spec.rb
+++ mastermind/spec/human_player_spec.rb
@@ -8,14 +8,14 @@ describe HumanPlayer do
       arg = double()
       allow($stdout).to receive(:puts) # so won't print message in test
       allow($stdin).to receive(:gets).and_return("1111")
-      expect(test_player.make_guess(arg)).to match_array([1, 1, 1, 1])
+      expect(test_player.make_guess(arg)).to eq [1, 1, 1, 1]
     end
 
     it "returns the first valid input provided" do
       arg = double()
       allow($stdout).to receive(:puts)
       allow($stdin).to receive(:gets).and_return("aaaa", "1111")
-      expect(test_player.make_guess(arg)).to match_array([1, 1, 1, 1])
+      expect(test_player.make_guess(arg)).to eq [1, 1, 1, 1]
     end
   end
 
@@ -23,13 +23,13 @@ describe HumanPlayer do
     it "returns valid string converted to array of ints" do 
       allow($stdout).to receive(:puts)
       allow($stdin).to receive(:gets).and_return("6666")
-      expect(test_player.set_code).to match_array([6, 6, 6, 6])
+      expect(test_player.set_code).to eq [6, 6, 6, 6]
     end
 
     it "returns the first valid input provided" do
       allow($stdout).to receive(:puts)
       allow($stdin).to receive(:gets).and_return("not a code", "another not code", "9999", "1234")
-      expect(test_player.set_code).to match_array([1, 2, 3, 4])
+      expect(test_player.set_code).to eq [1, 2, 3, 4]
     end
   end
 end
